ca05a41f47ef460039085a04aab03256a0433358
Bianca Schoti <bia_schoti@hotmail.com>
2023-03-24 11:10:07 -0300

    Correções

--- src/app.js
+++ src/app.js
@@ -1,29 +1,24 @@
+const routes = require('./routes/index')
 const path = require("path")
 const express = require('express')
 const app = express()
+// captura na forma de objeto literal tudo o que vem de um formulário
+app.use(express.urlencoded({ extended: false }))
+// converte as informações em formato JSON
 app.use(express.json())
 
-const  routes = require('./routes/index')
-
- HEAD
 // instanciando como view engine
 app.set("view engine", "ejs")
 // instanciando pasta views
 app.set("views", path.resolve("src", "views"))
-//liberando acesso a pasta public
+// liberando acesso a pasta public
 app.use(express.static(path.resolve("public")))
 
-app.set("view engine","ejs")
-
-
-
+/**
+ * Rotas
+ */
 app.use(routes)
 
-
 app.listen(3000, () => {
-    console.log('Servidor rodando')
+  console.log('Servidor Rodando!')
 })
-
-
-
-                  
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,31 +1,39 @@
 const express = require('express')
 const router = express.Router()
 
-const main = require ('../controllers/MainControler')
-
+const mainController = require('../controllers/MainController')
 const productController = require('../controllers/ProductController')
 
 // # Main
-//GET ALL
-app.get('/', mainControler.index )
+// GET ALL
+router.get('/', mainController.index)
+// GET ALL
+router.get('/search', mainController.search)
 
+// # Product
+// GET - EJS Detail - View
+router.get('/product/detail/:id', productController.detailEJS)
+// GET - EJS Create Form - View
+router.get('/product/create', productController.createFormEJS)
+// GET - EJS Update Form - View
+router.get('/product/update/:id', productController.updateFormEJS)
+// POST - EJS Create
+router.post('/product', productController.createEJS)
+// PUT - EJS Update
+router.put('/product/:id', productController.updateEJS)
+// DELETE - EJS Delete
+router.delete('/product/:id', productController.deleteEJS)
 
-//produtos
-
-//# GET ALL
-app.get('/pessoa', productController.todosProdutos )
-
-//# GET by ID
-app.get('/pessoa/:id', productController.produtosByID)
-//# POST
-app.post('/pessoa',productController.create )
-
-//# PUT
-app.put('/pessoa/:id',productController.update )
-
-//# DELETE
-app.delete('/pessoa/:id',productController.delete )
-
-
+// // # Product
+// // GET ALL
+// router.get('/product', productController.showAll)
+// // GET By Id
+// router.get('/product/:id', productController.showById)
+// // POST
+// router.post('/product', productController.create)
+// // PUT
+// router.put('/product/:id', productController.update)
+// // DELETE
+// router.delete('/product/:id', productController.delete)
 
 module.exports = router
