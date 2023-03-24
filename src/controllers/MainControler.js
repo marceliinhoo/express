6391331665bfbe22a3c14ce3fd9aca1be452dc1f
Bianca Schoti <bia_schoti@hotmail.com>
2023-03-24 16:15:45 -0300

    exclusão

--- src/controllers/MainControler.js
+++
@@ -1,8 +0,0 @@
-const products = require('../database/products.json')
-
-const MainControler = {
-    index: ( req, res) => {
-        res.render('index', { products })
-    }
-}
-module.exports = MainControler
