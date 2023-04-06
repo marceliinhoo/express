ad33431a6535cc79383e00ff063984d9ae388761
Gerson Sinohara <gerson.sinohara@telefonica.com>
2023-04-06 00:02:52 -0300

    instancia das views para o root do projeto no app.js e transferencia da pasta public para o root do projeto também para poder renderizar os htmls/ejs

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const path = require('path');
 
 const mainController = require('../controllers/MainController')
 const productController = require('../controllers/ProductController')
